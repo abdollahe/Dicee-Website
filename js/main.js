16c8bd740333917663f86897713b6da30fccf458
abdollahebadi <abdollah.ebadi@gmail.com>
2019-01-23 14:44:53 +1100

    logic added to change header

--- js/main.js
+++ js/main.js
@@ -14,6 +14,19 @@ function returnRandom() {
 function rollDices() {
   var number1 = returnRandom() ;
   document.querySelector(".img1").src = "images/" + imageList[number1 - 1] ;
-  number1 = returnRandom() ;
-  document.querySelector(".img2").src = "images/" + imageList[number1 - 1] ;
+  var number2 = returnRandom() ;
+  document.querySelector(".img2").src = "images/" + imageList[number2 - 1] ;
+
+  if( number1 === number2) {
+    document.querySelector("h1").textContent = "Draw" ;
+  }
+  else if (number1 > number2) {
+    document.querySelector("h1").textContent = "Player1 Wins!" ;
+  }
+  else {
+    document.querySelector("h1").textContent = "Player2 Wins!" ;
+  }
+
 }
+
+rollDices() ;
